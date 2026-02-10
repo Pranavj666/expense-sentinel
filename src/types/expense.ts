7e633baa345486f39b492b67f1f2dfcacefeec78
Pranav J <pj268@snu.edu.in>
2026-02-10 14:10:16 +0530

    Create Expense types interface file

---
+++ src/types/expense.ts
@@ -0,0 +1,17 @@
+// src/types/expense.ts
+
+export interface Expense {
+  id: string;
+  description: string;
+  amount: number;
+  category: string;
+  date: string;
+}
+
+export interface ExpenseContextType {
+  expenses: Expense[];
+  addExpense: (expense: Omit<Expense, 'id'>) => void;
+  deleteExpense: (id: string) => void;
+  getTotalExpenses: () => number;
+  getHighValueExpenses: () => Expense[];
+}
